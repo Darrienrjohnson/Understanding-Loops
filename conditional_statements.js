f60be837defaa0ca7ab555ef07362f926a6ebe82
Darrienrjohnson <69940765+Darrienrjohnson@users.noreply.github.com>
2021-06-12 23:03:51 -0400

    conditional statements
    understanding decision making in code conditional statements if else statements

---
+++ conditional_statements.js
@@ -0,0 +1,44 @@
+/*  Conditional Statement create decision making in your code. 
+"if" statements
+"else if" statements
+"else"statements
+
+  if (condition) {
+    code to run if condition is true
+  } else if (condition){
+    code to run if this condition is true
+  }else {
+    run some other code instead
+  }
+
+*Another example:
+
+let shoppingDone=false;
+let childsAllowance;
+
+if (shoppingDone===true){
+  childsAllowance = 10;
+} else {
+  childsAllowance = 5;
+}
+
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*/
